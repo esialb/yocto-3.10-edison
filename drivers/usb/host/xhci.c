a55d204b4ddd3c6ba687d2cfdad119e1208d3d17
Xenia Ragiadakou <burzalodowa@gmail.com>
2013-08-06 07:52:47 +0300

    xhci: add xhci_address_ctx trace event
    This patch defines a new event class, called xhci_log_ctx,
    that records in the ring buffer the context data, the
    context type (input or output), the context dma and virtual
    addresses, the context endpoint entries, the slot ID and
    whether the xHC uses 64 byte context data structures.
    
    This information can be used, later, to parse and display
    the context data fields with the appropriate plugin using
    the trace-cmd tool.
    
    Also, this patch defines a trace event, called xhci_address_ctx,
    to trace the contexts related to the Address Device command and
    adds the associated tracepoints in xhci_address_device().
    
    Signed-off-by: Xenia Ragiadakou <burzalodowa@gmail.com>
    Signed-off-by: Sarah Sharp <sarah.a.sharp@linux.intel.com>
    (cherry picked from commit 1d27fabec068a204186c6af10e05f23911c0c902)
    
    Signed-off-by: Ng Wei Tee <wei.tee.ng@intel.com>

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -3754,6 +3754,8 @@ int xhci_address_device(struct usb_hcd *hcd, struct usb_device *udev)
 
 	xhci_dbg(xhci, "Slot ID %d Input Context:\n", udev->slot_id);
 	xhci_dbg_ctx(xhci, virt_dev->in_ctx, 2);
+	trace_xhci_address_ctx(xhci, virt_dev->in_ctx,
+				slot_ctx->dev_info >> 27);
 
 	spin_lock_irqsave(&xhci->lock, flags);
 	cmd_trb = xhci_find_next_enqueue(xhci->cmd_ring);
@@ -3810,6 +3812,7 @@ int xhci_address_device(struct usb_hcd *hcd, struct usb_device *udev)
 				"code 0x%x.\n", virt_dev->cmd_status);
 		xhci_dbg(xhci, "Slot ID %d Output Context:\n", udev->slot_id);
 		xhci_dbg_ctx(xhci, virt_dev->out_ctx, 2);
+		trace_xhci_address_ctx(xhci, virt_dev->out_ctx, 1);
 		ret = -EINVAL;
 		break;
 	}
@@ -3830,6 +3833,8 @@ int xhci_address_device(struct usb_hcd *hcd, struct usb_device *udev)
 			(unsigned long long)virt_dev->out_ctx->dma);
 	xhci_dbg(xhci, "Slot ID %d Input Context:\n", udev->slot_id);
 	xhci_dbg_ctx(xhci, virt_dev->in_ctx, 2);
+	trace_xhci_address_ctx(xhci, virt_dev->in_ctx,
+				slot_ctx->dev_info >> 27);
 	xhci_dbg(xhci, "Slot ID %d Output Context:\n", udev->slot_id);
 	xhci_dbg_ctx(xhci, virt_dev->out_ctx, 2);
 	/*
@@ -3837,6 +3842,8 @@ int xhci_address_device(struct usb_hcd *hcd, struct usb_device *udev)
 	 * address given back to us by the HC.
 	 */
 	slot_ctx = xhci_get_slot_ctx(xhci, virt_dev->out_ctx);
+	trace_xhci_address_ctx(xhci, virt_dev->out_ctx,
+				slot_ctx->dev_info >> 27);
 	/* Use kernel assigned address for devices; store xHC assigned
 	 * address locally. */
 	virt_dev->address = (le32_to_cpu(slot_ctx->dev_state) & DEV_ADDR_MASK)
