61c24136c90ddbe4fe474694df95d3b8f184708a
robin <robin.kirkman@gmail.com>
2016-01-05 01:00:39 -0500

    Change collect script

--- EDISON/collect.sh
+++ EDISON/collect.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-if [ ! -f ./EDISON/collect-modules.sh ]; then
+if [ ! -f ./EDISON/collect.sh ]; then
   echo "please run from the root of a Yocto kernel repo"
   exit 1
 fi
 
 RELEASE=$(cat include/generated/utsrelease.h | awk '{print $3}' | perl -p -e 's/^"(.*)"$/$1/')
-MODULES="EDISON/modules/${RELEASE}"
+
+FSROOT="EDISON/fsroot/${RELEASE}"
+
+MODULES="${FSROOT}/lib/modules/${RELEASE}"
+
+BZIMAGE="arch/x86/boot/bzImage"
+
+echo "copying kernel modules"
 
 [ -e "${MODULES}" ] && rm -Rf "${MODULES}"
 
@@ -15,16 +22,26 @@ for KO in `find * -name '*.ko' | egrep -v -e '^EDISON/'`; do
   D=$(dirname "${KO}")
   mkdir -p "${MODULES}/kernel/${D}"
   cp "${KO}" "${MODULES}/kernel/${D}"
-  echo "${KO}"
+  echo -n .
   MODCOUNT=$((${MODCOUNT} + 1))
 done
 for KO in `find EDISON/extra -name '*.ko' | sed 's/^EDISON\/extra\///'`; do
   D=$(dirname "${KO}")
   mkdir -p "${MODULES}/extra/${D}"
   cp "EDISON/extra/${KO}" "${MODULES}/extra/${D}"
-  echo "${KO}"
+  echo -n .
   MODCOUNT=$((${MODCOUNT} + 1))
 done
 echo
 echo "${MODCOUNT} modules copied"
 
+echo "computing module dependencies"
+cp modules.* "${MODULES}"
+depmod -a -b "${FSROOT}" -F System.map "${RELEASE}"
+
+echo "copying kernel image"
+
+mkdir -p "${FSROOT}/lib/kernel"
+[ -e "${FSROOT}/boot/bzImage-${RELEASE}" ] && rm -Rf "${FSROOT}/boot/bzImage-${RELEASE}"
+cp "${BZIMAGE}" "${FSROOT}/lib/kernel/bzImage-${RELEASE}"
+cp "${BZIMAGE}" "${FSROOT}/boot/vmlinuz"
